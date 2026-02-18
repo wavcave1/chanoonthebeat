8b4336892ad75b42cc0a098922f8f4f337db7b74
Ian Crosby <wavcavemgmt@gmail.com>
2026-02-18 16:50:12 +0000

    2026

--- my-ap/src/components/Footer.js
+++ my-ap/src/components/Footer.js
@@ -44,7 +44,7 @@ function Footer() {
         </div>
 
         <div className="footer-bottom">
-          <p>&copy; 2024 Chano On The Beat. All rights reserved.</p>
+          <p>&copy; 2026 Chano On The Beat. All rights reserved.</p>
         </div>
       </div>
     </footer>
