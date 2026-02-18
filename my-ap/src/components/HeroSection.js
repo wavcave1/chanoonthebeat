92cebff60a40953a497931750bef480a45cad9de
Ian Crosby <wavcavemgmt@gmail.com>
2026-02-18 22:04:26 +0000

    bio

--- my-ap/src/components/HeroSection.js
+++ my-ap/src/components/HeroSection.js
@@ -57,7 +57,8 @@ function HeroSection() {
               </div>
             ))
           ) : (
-            <p>Chanoonthebeat is a talented rap producer from Nashville, known for his hard-hitting, signature sound that blends Southern trap energy with polished production. He has worked with prominent artists like Trapperman Dale, Eddie Valero, and a growing roster of talent, earning a reputation for crafting beats that elevate any project. With a relentless work ethic and a passion for creating music that resonates, Chano is quickly becoming a name to watch in the hip-hop scene.</p>
+            <p classname = 
+            Chanoonthebeat is a talented rap producer from Nashville, known for his hard-hitting, signature sound that blends Southern trap energy with polished production. He has worked with prominent artists like Kenny Muney, Trapperman Dale, Eddie Valero, and a growing roster of talent, earning a reputation for crafting beats that elevate any project. With a relentless work ethic and a passion for creating music that resonates, Chano is quickly becoming a name to watch in the hip-hop scene.</p>
           )}
         </div>
       </div>
