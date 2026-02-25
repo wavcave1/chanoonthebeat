0b7860c39c065fed655ebe3d80cdca681ced9b60
Ian Crosby <wavcavemgmt@gmail.com>
2026-02-25 01:09:58 -0600

    Update Bio.js

--- my-ap/src/components/Bio.js
+++ my-ap/src/components/Bio.js
@@ -9,11 +9,11 @@ function Bio() {
           <img className="bio-image" src= "/chano.jpg" alt="Chano On The Beat" />
           <div className="bio-copy">
             <p className="bio-text">
-           In a city renowned for its rich musical legacy, ChanoOnTheBeat is quickly establishing himself as a driving force in Nashville’s evolving hip-hop landscape. The Nashville-based producer is recognized for crafting cinematic soundscapes anchored by commanding drum patterns and a seamless fusion of Southern trap energy with polished, industry-caliber production. His work strikes a rare balance between raw authenticity and refined precision, delivering records that resonate in the streets while meeting the highest commercial standards.
+           In a city renowned for its rich musical legacy, Chano On The Beat is quickly establishing himself as a driving force in Nashville’s evolving hip-hop landscape. The Nashville based producer is recognized for crafting cinematic soundscapes anchored by commanding drum patterns and a seamless fusion of Southern trap energy with polished, industry caliber production. His work strikes a rare balance between raw authenticity and refined precision, delivering records that resonate in the streets while meeting the highest commercial standards.
         </p>
 
         <p className="bio-text">
-        With a steadily expanding catalog, Chano has collaborated with artists including Kenny Muney, Trapperman Dale, Eddie Valero, Starlito, Don Trip, and Lil Migo, solidifying his presence among respected Southern voices. More than a producer, he serves as a creative partner who shapes records around each artist’s strengths while maintaining a distinct and recognizable sonic identity. As his placements continue to grow, ChanoOnTheBeat represents a new generation of disciplined Southern producers building with intention, longevity, and undeniable momentum.
+        With a steadily expanding catalog, Chano has collaborated with artists including Kenny Muney, Trapperman Dale, Eddie Valero, Starlito, Don Trip, and Lil Migo, solidifying his presence among respected Southern voices. More than a producer, he serves as a creative partner who shapes records around each artist’s strengths while maintaining a distinct and recognizable sonic identity. As his placements continue to grow, Chano On The Beat represents a new generation of disciplined Southern producers building with intention, longevity, and undeniable momentum.
         </p>
           </div>
         </div>
